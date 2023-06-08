a07eee1868f5ef9baf31078f5fe15322584475fb
Hwclub1105 <9504.liao@gmail.com>
2023-06-08 15:13:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import customtkinter as tk
-root = tk.CTk()
+import tkinter as tk
+root = tk.Tk()
 root.title('Trignonometry')
 root.geometry('600x200+20+4')
 message = tk.Label(root, text="Hello World")
